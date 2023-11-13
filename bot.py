ce6a912d8621245a1b884e3667a0a39e8641b188
Succ <Kalle170512@gmail.com>
2023-11-13 21:58:38 +0100

    Bot can now detect slurs

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #import required dependencies
 import discord
 from discord.ext import commands
-
+import time
 
 
 intents = discord.Intents.default()
@@ -28,6 +28,23 @@ async def on_member_join(member):
     channel = client.get_channel(1173384679471202375)
     await channel.send(f"{member} HaS LeFt ThE CaVe :(, Press F to pay respects")
 
-@client.event
+
+with open("SlurList.txt") as file: # bad-words.txt contains one blacklisted phrase per line
+    bad_words = [bad_word.strip().lower() for bad_word in file.readlines()]
+
+@client.event    
 async def on_message(message):
-    print(message)
+    if message.author.bot:
+        return
+    print(message.content) #prints messages in console
+    for bad_word in bad_words:
+        if bad_word in message.content:
+            await message.delete() #delete said message
+            await message.channel.send("Slurs are strictly against the servers rules therefore you will be banned in")
+            time.sleep(1)
+            await message.channel.send("3")
+            time.sleep(1)
+            await message.channel.send("2")
+            time.sleep(1)
+            await message.channel.send("1")
+            
